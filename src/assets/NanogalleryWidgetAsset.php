eee13633a7e9fa5a5245c85daacee31a41cec425
semenov <semenov@skeeks.com>
2019-02-06 14:06:41 +0300

    dev

--- src/assets/NanogalleryWidgetAsset.php
+++ src/assets/NanogalleryWidgetAsset.php
@@ -9,11 +9,26 @@
 namespace skeeks\yii2\nanogalleryWidget\assets;
 
 use yii\web\AssetBundle;
+use yii\web\YiiAsset;
 
 /**
  * @author Semenov Alexander <semenov@skeeks.com>
  */
 class NanogalleryWidgetAsset extends AssetBundle
 {
+    public $sourcePath = '@skeeks/yii2/nanogalleryWidget/assets/src';
+
+    public $css = [
+        'vendor/nanogallery-5.10.3/dist/css/nanogallery.min.css',
+        'vendor/nanogallery-5.10.3/dist/css/themes/light/nanogallery_light.min.css',
+    ];
+
+    public $js = [
+        'vendor/nanogallery-5.10.3/dist/jquery.nanogallery.min.js',
+    ];
+
+    public $depends = [
+        YiiAsset::class
+    ];
 
 }
